3f1d235a06e9f204b3878b673f175b95f902d33d
Satish Venkatakrishnan <satish860@gmail.com>
2023-02-07 11:04:43 +0530

    Added a Category Endpoint

---
+++ src/Learning.Gmail.Api/Filters/Categories/CategoryEndpoint.cs
@@ -0,0 +1,19 @@
+﻿using FastEndpoints;
+
+namespace Learning.Gmail.Api.Filters.Categories
+{
+    public class CategoryEndpoint : Endpoint<CategoryRequest>
+    {
+        public override void Configure()
+        {
+            Get("/api/mailsbyCategory");
+        }
+
+        public override async Task HandleAsync(CategoryRequest req, CancellationToken ct)
+        {
+            await SendAsync("Hello world",
+                             200,
+                             ct);
+        }
+    }
+}
---
+++ src/Learning.Gmail.Api/Filters/Categories/CategoryRequest.cs
@@ -0,0 +1,10 @@
+﻿using FastEndpoints;
+
+namespace Learning.Gmail.Api.Filters.Categories
+{
+    public class CategoryRequest
+    {
+        [FromClaim("email")]
+        public string? EmailId { get; set; }
+    }
+}
